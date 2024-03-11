1b53dc91f9fa861088c405009107448f80867a1f
ropa99 <rpahlin@hotmail.com>
2024-03-11 11:14:12 +0100

    Refactored Class name

---
+++ src/main/java/org/javarrayexercises/JavaArrayExercise1.java
@@ -0,0 +1,13 @@
+package org.javarrayexercises;
+
+public class JavaArrayExercise1 {
+
+    public static void main(String[] args) {
+
+        //1
+        int arr1[] = {1, 2, 3, 4, 5, 6};
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.println("Element" + "[" + i + "]" + " Number: " + arr1[i]);
+        }
+    }
+}
